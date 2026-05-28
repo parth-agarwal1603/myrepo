8768bf21da550022714d8965fdda50d609b099ad
Parth Agarwal <parthagarwal16305@gmail.com>
2026-05-28 18:08:26 +0530

    trying new thing

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Git and C++ are working successfully!" << endl;
+
+    int a = 10;
+    int b = 20;
+
+    cout << "Sum = " << a + b << endl;
+
+    return 0;
+}

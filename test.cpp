68b9dcb994c6d188ddcd0d67c88500e19e28a39e
Parth Agarwal <parthagarwal16305@gmail.com>
2026-05-29 15:11:21 +0530

    this is changed second time

--- test.cpp
+++ test.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main() {
-    cout << "Git and C++ are working successfully!" << endl;
+    cout << "changes are working successfully!" << endl;
 
     int a = 10;
     int b = 20;
 
-    cout << "Sum = " << a + b << endl;
+    cout << "Summation = " << a + b << endl;
 
     return 0;
 }
